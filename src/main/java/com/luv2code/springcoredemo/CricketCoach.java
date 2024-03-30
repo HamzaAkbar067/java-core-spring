c0c628e53ac8015b6db258de6d467eef53d03f05
HamzaAkbar067 <hakbar067@gmail.com>
2024-03-30 05:26:02 +0100

    1st commit

---
+++ src/main/java/com/luv2code/springcoredemo/CricketCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes !!";
+    }
+
+    @Override
+    public Number getNumber() {
+        return 132132;
+    }
+
+}
